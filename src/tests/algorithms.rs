f924c769c931c0768eb624549b45a0b69b2c5d97
erik9631 <erik9631@gmail.com>
2024-08-11 12:47:55 +0200

    refactor: Changed layer tests

--- src/tests/algorithms.rs
+++ src/tests/algorithms.rs
@@ -1,8 +1,11 @@
+use eta_algorithms::data_structs::array::Array;
+use eta_algorithms::data_structs::queue::Queue;
+use eta_algorithms::data_structs::stack::Stack;
 use crate::algorithms::general::{bfs, dfs};
 use crate::algorithms::general::ControlFlow::{End, Resume};
-use crate::algorithms::max_flow::{DinicGraph};
+use crate::algorithms::max_flow::{DinicGraph, mark_levels};
 use crate::graph::Graph;
-use crate::handles::types::{Weight};
+use crate::handles::types::{Edge, Weight};
 use crate::handles::{vh, vh_pack, wgt};
 use crate::traits::{EdgeStore, GraphOperate, WeightedGraphOperate};
 use crate::weighted_graph::WeightedGraph;
@@ -182,45 +185,48 @@ pub fn graph_dfs_end_test(){
 
 }
 
-// #[test]
-// pub fn level_test(){
-//     let mut weighted_graph = WeightedGraph::new();
-//     // Write test for layering
-//     let a = weighted_graph.graph.create("a", 2);
-//     let a_a = weighted_graph.create_and_connect_weighted(a, "a_a", 1, 100);
-//     let a_a_a = weighted_graph.create_and_connect_weighted(a_a, "a_a_a", 1, 20);
-//     let a_a_x = weighted_graph.create_and_connect_weighted(a_a_a, "a_a_x", 1, 30);
-//
-//     let a_b = weighted_graph.create_and_connect_weighted(a, "a_b", 20, 3);
-//     let a_b_a = weighted_graph.create_and_connect_weighted(a_b, "a_b_a", 10, 1);
-//     let a_b_b = weighted_graph.create_and_connect_weighted(a_b, "a_b_b", 10, 1);
-//     let a_b_c = weighted_graph.create_and_connect_weighted(a_b, "a_b_c", 10, 1);
-//
-//     weighted_graph.graph.edge_storage.connect_weighted(a_b_a, a_a_x, 10);
-//     weighted_graph.graph.edge_storage.connect_weighted(a_b_b, a_a_x, 10);
-//     weighted_graph.graph.edge_storage.connect_weighted(a_b_c, a_a_x, 10);
-//
-//     let mut dinic_graph = DinicGraph::from(&weighted_graph.graph.vertices, &weighted_graph.graph.edge_storage, a, a_a_x);
-//     dinic_graph.mark_levels(a, a_a_x).expect("Sink not found");
-//
-//     let mut snap = vec![
-//         ("a_a_x".to_string(), 3),
-//         ("a_b_c".to_string(), 2),
-//         ("a_b_b".to_string(), 2),
-//         ("a_b_a".to_string(), 2),
-//         ("a_a_a".to_string(), 2),
-//         ("a_b".to_string(), 1),
-//         ("a_a".to_string(), 1),
-//         ("a".to_string(), 0),
-//     ];
-//
-//     bfs(&mut dinic_graph.edge_storage, vh_pack(a), dinic_graph.vertices.len(), |v_handle, layer|{
-//         let snap_data = snap.pop().unwrap();
-//         assert_eq!(dinic_graph.vertices[vh(*v_handle)], snap_data.0);
-//         assert_eq!(dinic_graph.layer_data[vh(*v_handle) as usize], snap_data.1);
-//         Resume
-//     });
-// }
+#[test]
+pub fn level_test(){
+    let mut weighted_graph = WeightedGraph::new();
+    // Write test for layering
+    let a = weighted_graph.graph.create("a", 2);
+    let a_a = weighted_graph.create_and_connect_weighted(a, "a_a", 1, 100);
+    let a_a_a = weighted_graph.create_and_connect_weighted(a_a, "a_a_a", 1, 20);
+    let a_a_x = weighted_graph.create_and_connect_weighted(a_a_a, "a_a_x", 1, 30);
+
+    let a_b = weighted_graph.create_and_connect_weighted(a, "a_b", 20, 3);
+    let a_b_a = weighted_graph.create_and_connect_weighted(a_b, "a_b_a", 10, 1);
+    let a_b_b = weighted_graph.create_and_connect_weighted(a_b, "a_b_b", 10, 1);
+    let a_b_c = weighted_graph.create_and_connect_weighted(a_b, "a_b_c", 10, 1);
+
+    weighted_graph.graph.edge_storage.connect_weighted(a_b_a, a_a_x, 10);
+    weighted_graph.graph.edge_storage.connect_weighted(a_b_b, a_a_x, 10);
+    weighted_graph.graph.edge_storage.connect_weighted(a_b_c, a_a_x, 10);
+
+    let mut flow_data = Array::new_default_bytes(weighted_graph.graph.vertices.len(), 0);
+    let mut flags = Array::new_default_bytes(weighted_graph.graph.vertices.len(), 0);
+    let mut queue = Queue::<*mut Edge>::new_pow2_sized(weighted_graph.graph.vertices.len()); // Direct pointer access is faster than offsets
+    let mut edges_copy = weighted_graph.graph.edge_storage.clone();
+    mark_levels(a, a_a_x, &mut edges_copy, &mut queue, &mut flags, &mut flow_data).expect("Sink not found");
+
+    let mut snap = vec![
+        ("a_a_x".to_string(), 3),
+        ("a_b_c".to_string(), 2),
+        ("a_b_b".to_string(), 2),
+        ("a_b_a".to_string(), 2),
+        ("a_a_a".to_string(), 2),
+        ("a_b".to_string(), 1),
+        ("a_a".to_string(), 1),
+        ("a".to_string(), 0),
+    ];
+
+    bfs(&mut edges_copy, vh_pack(a), weighted_graph.graph.vertices.len(), |v_handle, layer|{
+        let snap_data = snap.pop().unwrap();
+        assert_eq!(weighted_graph.graph.vertices[vh(*v_handle)], snap_data.0);
+        assert_eq!(flow_data[vh(*v_handle) as usize], snap_data.1);
+        Resume
+    });
+}
 
 #[test]
 pub fn dinic_test(){
