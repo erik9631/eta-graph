ff76894f3af021e25e199caed9d3234d510627d9
erik9631 <erik9631@gmail.com>
2024-08-18 12:32:50 +0200

    docs: Added config info to readme

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 use std::env;
 
 fn main() {
-    let msize_type = env::var("MSize").unwrap_or("u32".to_string());
+    let msize_type = env::var("MSize").unwrap_or("u64".to_string());
     println!("cargo:rustc-cfg=msize_type=\"{}\"", msize_type);
 }
